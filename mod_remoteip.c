c831ccb33fb0707814420b9a9ceb674b6a916de5
Schlomo Schapiro <github@schlomo.schapiro.org>
2015-02-27 14:05:17 +0100

    Show info message only in debug level

--- mod_remoteip.c
+++ mod_remoteip.c
@@ -445,7 +445,7 @@ ditto_request_rec:
                            conn->proxy_ips);
     }
 
-    ap_log_rerror(APLOG_MARK, APLOG_INFO|APLOG_NOERRNO, 0, r,
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG|APLOG_NOERRNO, 0, r,
                   conn->proxy_ips 
                       ? "Using %s as client's IP by proxies %s"
                       : "Using %s as client's IP by internal proxies",
