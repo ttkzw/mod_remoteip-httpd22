424679499e51c4c66b3eaecef26ee52f50fa934e
Joey <joey.bartlett@gmail.com>
2015-07-21 11:27:47 +0100

    ASL
    Do something about that Apache license clause.

--- mod_remoteip.c
+++ mod_remoteip.c
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * NOTICE:
+ * This file has been modified and is a derivative work.
  */
 
 #include "ap_config.h"
